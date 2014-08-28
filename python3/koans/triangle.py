987d46ce0bacc00988d8ad95b40bf9568bddda6b
Michael Hsueh <michael.k.hsueh@gmail.com>
2014-08-28 01:47:58 -0700

    finished lesson 15

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,12 +17,22 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    validateSides(a, b, c)        
     if a == b == c :
         return 'equilateral'
-    elif a != b != c != a: 
+    elif a != b != c != a : 
         return 'scalene'
     else : 
         return 'isosceles'
+
+def validateSides(a, b, c):
+    sides = (a, b, c)
+    if 0 in sides :
+        raise TriangleError('sides cannot be of length 0')
+    if not all(n > 0 for n in sides) :
+        raise TriangleError('side cannot be of negative length')
+    if sides[0] + sides[1] < sides[2] or sides[1] + sides[2] < sides[0] or sides[0] + sides[2] < sides[1] :
+        raise TriangleError('invalid combination of side lengths')
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
